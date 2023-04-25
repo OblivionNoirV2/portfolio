31a4e192e59f30d8d81e9e797fdc3f57722a818a
OblivionNoirV2 <donahueb85@gmail.com>
2023-04-25 15:08:10 -0500

    initial

---
+++ src/showtime.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sparkles.css';
+//import './index.css';
+function App() {
+  return (
+    <div className="text-2xl">
+      stuff
+    </div>
+  );
+}
+
+export default App;
