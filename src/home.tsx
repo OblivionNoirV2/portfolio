db30a415250446085b73336f800e2969e561ed10
OblivionNoirV2 <donahueb85@gmail.com>
2023-04-27 18:16:21 -0500

    minor styling updates

--- src/home.tsx
+++ src/home.tsx
@@ -1,11 +1,13 @@
 //Every other word goes to the end of the container
+
 const NameSegment = () => {
     return (
         <ul className="flex flex-col space-y-8 name-box ml-36 
-        mt-12 text-zinc-200 max-w-sm">
+        mt-12 text-zinc-200  w-1/5">
             <li className="flex text-7xl">Benjamin</li>
             <li className="flex text-6xl">Donahue</li>
-            <li className="flex text-4xl">Fullstack</li>
+            <li className="flex text-4xl">Full</li>
+            <li className="flex text-4xl">Stack</li>
             <li className="flex text-4xl">Web</li>
             <li className="flex text-4xl">Developer</li>
         </ul>
