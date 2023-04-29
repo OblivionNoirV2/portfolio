be13bd1a3a50244441857ca12860dd4f3abd2ba0
OblivionNoirV2 <donahueb85@gmail.com>
2023-04-28 23:17:57 -0500

    pictures, minor edits

--- src/about.tsx
+++ src/about.tsx
@@ -2,28 +2,43 @@ import { Link } from "react-router-dom";
 const About = () => {
     //make the h1 look really fancy 
     return (
-        <main className='flex flex-col w-1/2 m-auto text-2xl not-shadow '>
-            <h1>Hi, I'm Ben</h1>
-            <p>
-                I'm a Front End Web Developer with a strong passion for
-                computer science, programming and art. I don't yet have any
-                experience in the field, I but love what I do and am eager to learn
-                and build some awesome things!
-                You can see what I've built <Link to="/projects" className="link_">here</Link>.
-            </p>
-
-            Some other interests of mine include:
-            <ol>
-                <li className="no-hover">&#9734;Music: I play guitar and have been very passionate
-                    about all kinds of music ever since I was little.</li>
-                <li className="no-hover">&#9734;Gaming: Building a gaming PC is actually what created
-                    an interest in computer science and brought me here!</li>
-                <li className="no-hover">&#9734;Anything that makes you think. I'm very interested in psychology
-                    and media that makes you use your head.
-                    My inclination for that combined with my interest in artistry
-                    and creation made front end web a good fit for me.</li>
-            </ol>
+        <>
+            <section className='flex flex-col w-2/6 ml-36 pl-4 text-2xl 
+        not-shadow leading-loose'>
+                <h1 className="flex justify-start text-5xl">Hi, I'm Ben</h1>
+                <p className="mt-8">
+                    I'm a primarily self-taught Front End Web Developer with a strong passion for
+                    computer science, programming and art. I don't yet have any
+                    experience in the field, I but love what I do and am eager to learn
+                    and build some awesome things!
+                    You can see what I've built <Link to="/projects" className="link_">here</Link>.
+                </p>
+                <h2 className="mt-6">Some other interests of mine include:</h2>
+                <ol className="space-y-2 mt-2">
+                    <li className="no-hover"><span className="text-4xl pr-0.5">&#9734;</span>Music: I play guitar and have been very passionate
+                        about all kinds of music ever since I was little.</li>
+                    <li className="no-hover"><span className="text-4xl pr-0.5">&#9734;</span>Gaming: Building a gaming PC is actually what created
+                        an interest in computer science and brought me here!</li>
+                    <li className="no-hover"><span className="text-4xl pr-0.5">&#9734;</span>Anything that makes you think. I'm very interested in psychology
+                        and media that makes you use your head or tells a great story.
+                        My penchant for that combined with my interest in artistry
+                        and creation made front end web a good fit for me.</li>
+                </ol>
+            </section>
+            <section>
+                <Photos />
+            </section>
+        </>
+    );
+}
+//animated photo carousel, automatically scrolls at a timed interval
+//with short descriptions of each photo
+//don't forget the alt text!
+const Photos = () => {
+    return (
+        <main className="photo-c">
+            some stuff
         </main>
-    )
+    );
 }
 export default About;
