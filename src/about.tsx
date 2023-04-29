84c2968380cfe998522283c4516166462d713c64
OblivionNoirV2 <donahueb85@gmail.com>
2023-04-28 20:25:01 -0500

    styling

--- src/about.tsx
+++ src/about.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const About = () => {
     //make the h1 look really fancy 
     return (
-        <main>
+        <main className='flex flex-col w-1/2 m-auto text-2xl not-shadow '>
             <h1>Hi, I'm Ben</h1>
             <p>
                 I'm a Front End Web Developer with a strong passion for
@@ -11,19 +11,18 @@ const About = () => {
                 and build some awesome things!
                 You can see what I've built <Link to="/projects" className="link_">here</Link>.
             </p>
-            <p>
-                Some other interests of mine include:
-                <ul>
-                    <li className="no-hover">Music: I play guitar and have been very passionate
-                        about all kinds of music ever since I was little.</li>
-                    <li className="no-hover">Gaming. Building a gaming PC is actually what created
-                        an interest in computer science and brought me here!</li>
-                    <li className="no-hover">Anything that makes you think. I'm very interested in psychology
-                        and media that makes you use your head.
-                        My inclination for that combined with my interest in artistry
-                        and creation made front end web a good fit for me.</li>
-                </ul>
-            </p>
+
+            Some other interests of mine include:
+            <ol>
+                <li className="no-hover">&#9734;Music: I play guitar and have been very passionate
+                    about all kinds of music ever since I was little.</li>
+                <li className="no-hover">&#9734;Gaming: Building a gaming PC is actually what created
+                    an interest in computer science and brought me here!</li>
+                <li className="no-hover">&#9734;Anything that makes you think. I'm very interested in psychology
+                    and media that makes you use your head.
+                    My inclination for that combined with my interest in artistry
+                    and creation made front end web a good fit for me.</li>
+            </ol>
         </main>
     )
 }
