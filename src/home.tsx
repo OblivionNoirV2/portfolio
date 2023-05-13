00473e32b38fe2dc615a1dc5beb10fa14606a46d
OblivionNoirV2 <donahueb85@gmail.com>
2023-05-12 22:07:30 -0500

    add new homepage section

--- src/home.tsx
+++ src/home.tsx
@@ -1,47 +1,65 @@
-//Every other word goes to the end of the container
+//lets have 2 box shadows that line up, so it looks like a circuit 
 
+const ShortDesc = () => {
+    return (
+        <section className="w-5/12 flex shadow-name mt-24 rounded-r-xl">
+            <p className="text-2xl ml-16 mr-8 mb-8 leading-normal">
+                I'm a full stack web developer with a passion for
+                learning and creating. I have a love for creation and
+                being a part of modern technology, and testing the limits
+                of what we as programmers can create. During my lifetime I hope
+                to build some awesome things and help progress the field I love.
+            </p>
+        </section>
+    )
+}
+//Every word is below the prev and a little to the right
 const NameSegment = () => {
     return (
-        <main>
+        <main className="ml-28 flex w-full">
             <div className="p-[32rem] shadow-name w-8/12 
-            pl-4 rounded-xl -mt-16">
+            pl-4 rounded-xl -mt-16 m-auto">
                 <ul className="flex flex-row name-box -mt-48
                  text-zinc-200 pl-4 space-x-12 pt-4">
-                    <div className="transform translate-y-[2rem]">
-                        <li className="flex text-7xl self-start no-hover">
-                            Benjamin</li>
+                    <div className="transform translate-y-[2vw]">
+                        <li className="flex text-[2vw] no-hover">
+                            Benjamin
+                        </li>
                     </div>
-                    <div className="transform translate-y-[6rem]">
-                        <li className="flex text-6xl no-hover">
+                    <div className="transform translate-y-[4vw]">
+                        <li className="flex text-[2vw] no-hover">
                             Donahue
                         </li>
                     </div>
-                    <div className="transform translate-y-[12rem]">
-                        <li className="flex text-4xl no-hover">
+                    <div className="transform translate-y-[6vw]">
+                        <li className="flex text-[2vw] no-hover">
                             Full
                         </li>
                     </div>
-                    <div className="transform translate-y-[18rem]">
-                        <li className="flex text-4xl no-hover">
+                    <div className="transform translate-y-[8vw]">
+                        <li className="flex text-[2vw] no-hover">
                             Stack
                         </li>
                     </div>
-                    <div className="transform translate-y-[24rem]">
-                        <li className="flex text-4xl no-hover">
+                    <div className="transform translate-y-[10vw]">
+                        <li className="flex text-[2vw] no-hover">
                             Web
                         </li>
                     </div>
-                    <div className="transform translate-y-[30rem]">
-                        <li className="flex text-4xl no-hover">
+                    <div className="transform translate-y-[12vw]">
+                        <li className="flex text-[2vw] no-hover">
                             Developer
                         </li>
                     </div>
                 </ul>
             </div>
+            <section>
+                <ShortDesc />
+            </section>
         </main>
     );
 };
 
 
-//what else should I put here? Fancy animation maybe?
+
 export default NameSegment;
