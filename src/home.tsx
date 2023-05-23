18d4e4ff33fd45dab1f9b5c8b278fe410a9d9389
OblivionNoirV2 <donahueb85@gmail.com>
2023-05-23 11:25:43 -0500

    changing color theme

--- src/home.tsx
+++ src/home.tsx
@@ -1,65 +1,63 @@
 //lets have 2 box shadows that line up, so it looks like a circuit 
+import { Link } from "react-router-dom";
 
-const ShortDesc = () => {
+const GlassMenu = () => {
     return (
-        <section className="w-5/12 flex shadow-name mt-24 rounded-r-xl">
-            <p className="text-2xl ml-16 mr-8 mb-8 leading-normal">
-                I'm a front end web developer with a passion for
-                learning and creating. I have a love for creation and
-                being a part of modern technology, and testing the limits
-                of what we as programmers can create. During my lifetime I hope
-                to build some awesome things and help progress the field I love.
-            </p>
-        </section>
-    )
-}
+        <nav className='glass text-5xl rounded-2xl'>
+            <ul className=' flex flex-col items-center '>
+
+                <li className='add-hover mr-64 pt-12'>
+                    <Link to="/">Home</Link>
+                </li>
+
+                <li className='add-hover ml-64 pt-12'>
+                    <Link to="/about">About Me</Link>
+                </li>
+                <li className='add-hover mr-64 pt-12'>
+                    <Link to="/skills">Skills</Link>
+                </li>
+                <li className='add-hover ml-64 pt-12'>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+
 //Every word is below the prev and a little to the right
 const NameSegment = () => {
     return (
-        <main className="ml-28 flex w-full">
-            <div className="p-[32rem] shadow-name w-8/12 
-            pl-4 rounded-xl -mt-16 m-auto">
-                <ul className="flex flex-row name-box -mt-48
-                 text-zinc-200 pl-4 space-x-12 pt-4">
-                    <div className="transform translate-y-[2vw]">
-                        <li className="flex text-[2vw] no-hover">
-                            Benjamin
-                        </li>
-                    </div>
-                    <div className="transform translate-y-[4vw]">
-                        <li className="flex text-[2vw] no-hover">
-                            Donahue
-                        </li>
-                    </div>
-                    <div className="transform translate-y-[6vw]">
-                        <li className="flex text-[2vw] no-hover">
-                            Front
-                        </li>
-                    </div>
-                    <div className="transform translate-y-[8vw]">
-                        <li className="flex text-[2vw] no-hover">
-                            End
-                        </li>
-                    </div>
-                    <div className="transform translate-y-[10vw]">
-                        <li className="flex text-[2vw] no-hover">
-                            Web
-                        </li>
-                    </div>
-                    <div className="transform translate-y-[12vw]">
-                        <li className="flex text-[2vw] no-hover">
-                            Developer
-                        </li>
-                    </div>
-                </ul>
+
+        <main className="flex flex-row w-1/2 h-screen items-center">
+
+            <div className="flex flex-col items-start ml-64">
+                <h1 className="text-7xl">Welcome to my site</h1>
+                <p className="mt-8 w-full">
+                    <ShortDesc />
+                </p>
             </div>
-            <section>
-                <ShortDesc />
+
+            <section className=" w-1/2">
+                <GlassMenu />
             </section>
+
         </main>
+
     );
 };
 
 
 
+const ShortDesc = () => {
+    return (
+        <p className="text-2xl leading-loose w-2/3">
+            I am Benjamin Donahue, a front end web developer with a passion for
+            learning and creating. I have a love for creation and
+            being a part of modern technology, and testing the limits
+            of what we as programmers can create. During my lifetime I hope
+            to build some awesome things and help progress the field I love.
+        </p>
+    )
+}
 export default NameSegment;
--- src/showtime.tsx
+++ src/showtime.tsx
@@ -7,34 +7,10 @@ import TechAndProjects from './skills';
 import NameSegment from './home';
 
 //this will need to be rendered in a different location for mobile
-const NavBar = () => {
-  return (
-    <main>
-      <nav>
-        <ul className='flex justify-end space-x-8 mr-12 mt-4 
-        text-2xl text-slate-200'>
-          <li className='add-hover'>
-            <Link to="/">Home</Link>
-          </li>
-          <li className='add-hover'>
-            <Link to="/about">About Me</Link>
-          </li>
-          <li className='add-hover'>
-            <Link to="/skills">Skills</Link>
-          </li>
-          <li className='add-hover'>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </main>
-  );
-};
-//Navbar is always there, the rest changes 
+
 function App() {
   return (
     <>
-      <NavBar />
       <Routes>
         <Route path="/" element={<NameSegment />} />
         <Route path="/about" element={<About />} />
